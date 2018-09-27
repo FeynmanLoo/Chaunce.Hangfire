00a8e4984da0b8015a50c37ce6064be9d13f1613
Mingor <feynman0919@gmail.com>
2018-09-27 08:42:11 +0800

    Signed-off-by: Mingor <feynman0919@gmail.com>

--- src/Chaunce.Hangfire.Client/HttpJobItem.cs
+++ src/Chaunce.Hangfire.Client/HttpJobItem.cs
@@ -4,16 +4,33 @@ namespace Chaunce.Hangfire.Client
 {
     public class HttpJobItem
     {
+        public HttpJobItem()
+        {
+            Method = "Post";
+            ContentType = "application/json";
+            Timeout = 20000;
+            DelayFromMinutes = 15;
+        }
         /// <summary>
         /// 请求Url
         /// </summary>
         public string Url { get; set; }
 
         /// <summary>
+        /// 请求参数
+        /// </summary>
+        public string Method { get; set; }
+
+        /// <summary>
         /// 参数
         /// </summary>
-        public object Data { get; set; }
+        public string Data { get; set; }
 
+        public string ContentType { get; set; }
+
+        public int Timeout { get; set; }
+
+        public int DelayFromMinutes { get; set; }
         public string Corn { get; set; }
         public string JobName { get; set; }
 
